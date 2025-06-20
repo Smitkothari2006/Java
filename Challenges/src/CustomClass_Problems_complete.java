aaa5b722599285be08a8fe39cf2e5ca942a3c95a
Smit Kothari <saksmit06@gmail.com>
2025-06-20 13:34:02 +0530

    Add solutions for problems in CustomClass_Problems_complete.java and Methods_Problems_complete.java
    - CustomClass_Problems_complete.java: Completed solutions for problems related to custom class implementations.
    - Methods_Problems_complete.java: Implemented solutions for various method-related problems and exercises.

--- Challenges/src/CustomClass_Problems_complete.java
+++ Challenges/src/CustomClass_Problems_complete.java
@@ -1,4 +1,114 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+
+/* Problem 1
+class Employee{
+//    int salary;
+        String name;
+        public void GetSalary(int sal){
+//            salary=sal;
+            System.out.println("Salary of the Employee is "+sal);
+        }
+        public void GetName(){
+            System.out.println("Name of the employee is "+ name);
+        }
+
+        public void SetName(String n){
+            name = n;
+        }
+} */
+
+/* Problem 2
+class Square{
+    float side;
+    public void GetSide(float size){
+        side = size;
+    }
+
+    public void Area(){
+        System.out.println("Area of the Square is "+(side*side));
+    }
+    public void Perimeter(){
+        System.out.println("Perimeter of the Square is "+(side*4));
+    }
+}*/
+
+/*Problem 3
+class Rectangle{
+    float Length;
+    float Breath;
+    public void GetSide(float l,float b){
+        Length = l;
+        Breath = b;
+    }
+    public void Area(){
+        System.out.println("Area of the Rectangle is "+(Length*Breath));
+    }
+    public void Perimeter(){
+        System.out.println("Perimeter of the Rectangle is "+(2*(Length+Breath)));
+    }
+}*/
+
+// Problem 4
+class Circle{
+    float pi = 3.14159265359f;
+    float radius;
+    public void GetSide(float r){
+        radius = r;
+    }
+    public void Area(){
+        System.out.println("Area of the Circle is "+(pi*radius*radius));
+    }
+    public void Perimeter(){
+        System.out.println("Perimeter of the Circle is "+(2*pi*radius));
+    }
+}
+
 
 public class CustomClass_Problems_complete {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        /* Problem 1
+        Employee smit=new Employee();
+        System.out.println("Enter the salary of the Employee");
+        int sal = scan.nextInt();
+        scan.nextLine();
+        smit.GetSalary(sal);
+        System.out.println("Enter the salary of the Employee");
+        String name = scan.nextLine();
+        smit.SetName(name);
+        smit.GetName();
+         */
+
+
+        /* Problem 2
+        Square sq1 = new Square();
+        System.out.println("Enter the Side of the Square ");
+        float size = scan.nextFloat();
+        sq1.GetSide(size);
+        sq1.Area();
+        sq1.Perimeter();*/
+
+
+        /* Problem 3
+        Rectangle rect1 = new Rectangle();
+        System.out.println("Enter the Lenght of the Rectangle ");
+        float l = scan.nextFloat();
+        System.out.println("Enter the Breath of the Rectangle ");
+        float b = scan.nextFloat();
+        rect1.GetSide(l,b);
+        rect1.Area();
+        rect1.Perimeter();*/
+
+        //Problem 4
+        Circle Cir1 = new Circle();
+        System.out.println("Enter the Radius of the Circle ");
+        float Radius = scan.nextFloat();
+        Cir1.GetSide(Radius);
+        Cir1.Area();
+        Cir1.Perimeter();
+
+
+    }
 }
--- Challenges/src/Methods_Problems_complete.java
+++ Challenges/src/Methods_Problems_complete.java
@@ -1,4 +1,82 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Methods_Problems_complete {
+
+    public static void MultiplicationTable(int num ){
+        for (int i = 0; i <=10 ; i++) {
+            System.out.println(num + " X " + i + " = " + (num*i));
+        }
+    }
+
+    public static void Pattern(int n){
+        for (int i = 0; i <n; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void ReversePattern(int n){
+        for (int i = 0; i <n; i++) {
+            for (int j = i; j <=n-1; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int NaturalNumbers(int n){
+        if (n==1){
+            return 1;
+        }
+        else{
+            return n+NaturalNumbers(n-1);
+        }
+    }
+
+    public static int Fibonacci(int n){
+        /*
+        if (n==1){
+            return 0;
+        }
+        else if (n==2){
+            return 1;
+        }*/
+        if (n==1||n==2){
+            return n-1;
+        }
+        else {
+            return Fibonacci(n-1)+Fibonacci(n-2);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a Number whose Multiplication Table is to be known ");
+        int table = scan.nextInt();
+        MultiplicationTable( table );
+
+        System.out.println("Enter Number of Rows in pattern ");
+        int termPattern = scan.nextInt();
+        Pattern(termPattern);
+
+        System.out.println("Enter number of Natural Numbers whose sum is required");
+        int NumNatural = scan.nextInt();
+        int resultNatural = NaturalNumbers(NumNatural);
+        System.out.println("Sum of first  "+NumNatural+" Natural numbers is " + resultNatural);
+
+        System.out.println("Enter Number of Rows in pattern ");
+        int termReverse = scan.nextInt();
+        ReversePattern(termReverse);
+
+        System.out.println("Enter Nth term to find of Fibonacci series ");
+        int termFib = scan.nextInt();
+        int resultFib = Fibonacci(termFib);
+        System.out.println(termFib +"th Terms of fibonacci series is " + resultFib );
+
+
+
+    }
 }
